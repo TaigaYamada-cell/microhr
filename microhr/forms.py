d44419949ec40a9c628c6b3afd1398184050d30b
Taiga Yamada <daikonsukitarou@gmail.com>
2022-04-27 03:41:28 +0900

    フォームのフィールドにバリデーションを追加

--- microhr/forms.py
+++ microhr/forms.py
@@ -8,13 +8,30 @@ class WorkForm(forms.ModelForm):
     class Meta:
         model = Work
         fields = ('title', 'salary_min', 'salary_max', 'text')
-        labels = {
-            'title': '求人タイトル',
-            'salary_min': '給与下限(万円)',
-            'salary_max': '給与上限(万円)',
-            'text': '委細',
-        }
-
+        
+    title = forms.CharField(
+        label='求人タイトル',
+        max_length=128,
+        required=True,
+        help_text='必須',
+    )
+    salary_min = forms.IntegerField(
+        label='給与下限(万円)',
+        required=True,
+        min_value=13,
+        help_text='必須',
+        )
+    salary_max = forms.IntegerField(
+        label='給与上限(万円)',
+        required=True,
+        help_text='必須',
+        )        
+    text = forms.CharField(
+        label='委細',
+        widget=forms.Textarea,
+        required=False,
+        help_text='*任意',
+    )
 
 class WorkerProfileForm(forms.ModelForm):
     class Meta:
