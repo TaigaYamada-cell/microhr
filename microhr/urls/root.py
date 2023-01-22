bf9c2e018a6ee58b398a81bcb100eb0cc75c0b07
Taiga Yamada <daikonsukitarou@gmail.com>
2023-01-22 09:57:54 +0900

    first commit

--- microhr/urls/root.py
+++ microhr/urls/root.py
@@ -4,4 +4,5 @@ from microhr.views import root
 
 urlpatterns = [
     path('', root.home, name='home'),
+    path('api/users', root.getWorks, name='works')
 ]
--- microhr/views/root.py
+++ microhr/views/root.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from microhr.models import Work
 from logging import getLogger
+from django.core import serializers
+
 logger = getLogger(__name__)
 
 
@@ -10,3 +13,10 @@ def home(request):
     works = Work.objects.all()
     context = {"works": works}
     return render(request, "home.html", context)
+
+
+"""API用、求人全取得"""
+def getWorks(request):
+    works = serializers.serialize("json", Work.objects.all())
+    return HttpResponse(works)
+
